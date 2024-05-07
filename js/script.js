3f0c43eff3d027ad16b7d54c8572bbb015b5d176
dogukan_o <dogukan.ozgen@gmail.com>
2024-05-07 06:58:46 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// invite button
+const addGuestButton = document.querySelector(".invite");
+// label for the invite button
+const guestInputLabel = document.querySelector(".add-guest label");
+// text input box
+const guestInput = document.querySelector(".add-guest input");
+// unordered list (not yet visible)
+const guestList = document.querySelector(".guest-list");
+// span class for number of guests attending
+const guestCount = document.querySelector(".attendance");
+// alert when guest list is full (not yet visible)
+const guestFull = document.querySelector(".alert");
